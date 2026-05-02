d67cf8b517ba0c09b19b4b73db0fb18c532da775
kklasman <34178343+kklasman@users.noreply.github.com>
2026-05-02 17:38:51 -0400

    Add logging to file_utils.py get_geojson_filename function and utilities.py get_file_path_for_filename functions to help debug file not found errors (geojson files which were moved to subfolder) when app is deployed to both Streamlit Cloud and Render.

--- Lib/file_utils.py
+++ Lib/file_utils.py
@@ -1,9 +1,12 @@
 import os
 from streamlit import session_state as ss
 import utilities as u
+import logging_functions as lf
 
 def get_geojson_filename(selected_state):
     cwd = os.getcwd()
+    lf.logger.info(f'cwd: {cwd}')
+
     # file_name = st.session_state.geojson_files_dict[selected_state]
     # filename_column = 'geojson_filename' if st.session_state.selected_maptype == 'Town' else 'state_geojson_filename'
     # file_name = ss.wandrer_regions.query(f'State == "{selected_state}"')[filename_column].to_list()[0]
@@ -20,9 +23,8 @@ def get_geojson_filename(selected_state):
     if file_name is None:
         return ''
 
-    # file_path = os.path.join(cwd, r'data\10150\boundaries', file_name)
     file_path = u.get_filepath_for_filename(file_name)
+    lf.logger.info(f'file_path: {file_path}')
 
-    # file_path = os.path.join(cwd, r'data\boundaries', file_name)
     filesize = os.path.getsize(file_path)
     return file_path
--- Lib/utilities.py
+++ Lib/utilities.py
@@ -1,10 +1,11 @@
 import os
+import logging_functions as lf
 
 def get_filepath_for_filename(file_name):
     cwd = os.getcwd()
     file_path = os.path.join(cwd, 'data', 'boundaries', file_name)
     # file_path = os.path.join(cwd, 'Lib', 'data', 'boundaries', file_name)
-    print(f'file_path {file_path} exists {os.path.exists(file_path)}')
+    lf.logger.info(f'file_path {file_path} exists {os.path.exists(file_path)}')
     if not os.path.exists(file_path):
         # file lives in a different folder in development
         file_path = os.path.join(cwd, r'data\boundaries', file_name)
